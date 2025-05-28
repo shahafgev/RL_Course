440af6f9a279d03c99b42c6e85b8609a55fcdee8
shahafgev <gevs@post.bgu.ac.il>
2025-05-28 12:41:19 +0300

    Finish multi-arm bandit section

--- assignment2/main_Q1.py
+++ assignment2/main_Q1.py
@@ -8,89 +8,114 @@ import matplotlib.pyplot as plt
 script_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(script_dir)
 
-def run_random_policy(env, horizon):
+def run_random_policy(env):
     regret = []
     obs, done = env.reset(), False
     cumulative_regret = 0
-
-    for t in range(horizon):
-        arm = random.choice(env.action_space.param_space['drawn'])
-        action = {f'drawn({a})': a == arm for a in env.action_space.param_space['drawn']}
-        obs, reward, done, _ = env.step(action)
-        cumulative_regret -= reward
+    
+    while not done:
+        arm = random.choice(ARMS)
+        action = {f'roll___{a}': a == arm for a in ARMS}
+        obs, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
+        cumulative_regret += mu_star - reward
         regret.append(cumulative_regret)
 
+    # print("Finished random")
     return regret
 
-def run_greedy_policy(env, horizon):
-    arm_rewards = {a: [] for a in env.action_space.param_space['drawn']}
+def run_greedy_policy(env):
+    arm_rewards = {a: [] for a in ARMS}
+    obs, done = env.reset(), False
+    cumulative_regret = 0
+    regret = []
 
     # Explore each arm 100 times
-    for a in arm_rewards:
+    for a in ARMS:
         for _ in range(100):
-            env.reset()
-            action = {f'drawn({x})': x == a for x in arm_rewards}
-            obs, reward, done, _ = env.step(action)
-            arm_rewards[a].append(-reward)
+            action = {f'roll___{a}': True}
+            obs, reward, terminated, truncated, info = env.step(action)
+            done = terminated or truncated
+            arm_rewards[a].append(reward)
+            cumulative_regret += mu_star - reward
+            regret.append(cumulative_regret)
 
-    best_arm = max(arm_rewards, key=lambda a: np.mean(arm_rewards[a]))
+    estimated_means = {a: np.mean(arm_rewards[a]) for a in ARMS}
+    best_arm = max(estimated_means, key=estimated_means.get)
 
-    obs, done = env.reset(), False
-    cumulative_regret = 0
-    regret = []
-    for t in range(horizon):
-        action = {f'drawn({a})': a == best_arm for a in arm_rewards}
-        obs, reward, done, _ = env.step(action)
-        cumulative_regret -= reward
+    while not done:
+        action = {f'roll___{a}': a == best_arm for a in ARMS}
+        obs, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
+        cumulative_regret += mu_star - reward
         regret.append(cumulative_regret)
-
+    # print("Finished greedy")
     return regret
 
-def run_ucb1_policy(env, horizon):
-    n = len(env.action_space.param_space['drawn'])
+def run_ucb1_policy(env):
+    n = len(ARMS)
     counts = np.zeros(n)
     values = np.zeros(n)
-    arms = list(env.action_space.param_space['drawn'])
     regret = []
     cumulative_regret = 0
-
-    for t in range(horizon):
-        if t < n:
-            arm_idx = t
+    obs, done = env.reset(), False
+    t = 0 # Turn index
+    while not done:
+        if t < n: 
+            arm_idx = t # Set the arm to roll as the turn index
         else:
             upper_bounds = values + np.sqrt(2 * np.log(t + 1) / (counts + 1e-9))
             arm_idx = np.argmax(upper_bounds)
 
-        arm = arms[arm_idx]
-        action = {f'drawn({a})': a == arm for a in arms}
-        obs, reward, done, _ = env.step(action)
-        x = -reward
+        arm = ARMS[arm_idx]
+        action = {f'roll___{a}': a == arm for a in ARMS}
+        obs, reward, terminated, truncated, info = env.step(action)
+        done = terminated or truncated
         counts[arm_idx] += 1
-        values[arm_idx] += (x - values[arm_idx]) / counts[arm_idx] 
+        values[arm_idx] += (reward - values[arm_idx]) / counts[arm_idx] # Incremental mean update
 
-        cumulative_regret += x
+        cumulative_regret += mu_star - reward
         regret.append(cumulative_regret)
+        t += 1
 
+    # print("Finished ucb")
     return regret
 
-if __name__ == '__main__':
-    domain_file = "BanditSim.rddl"
-    instance_file = "BanditSim_100.rddl"
-    env = pyRDDLGym.make(domain=domain_file, instance=instance_file)
 
-    horizon = 20000
-    episodes = 1
+domain_file = "MAB_domain.rddl"
+instance_file = "MAB_instance.rddl"
+env = pyRDDLGym.make(domain=domain_file, instance=instance_file)
 
-    avg_random = np.mean([run_random_policy(env, horizon) for _ in range(episodes)], axis=0)
-    avg_greedy = np.mean([run_greedy_policy(env, horizon) for _ in range(episodes)], axis=0)
-    avg_ucb1 = np.mean([run_ucb1_policy(env, horizon) for _ in range(episodes)], axis=0)
+NUM_ARMS = 100
+EPISODES = 20
+ARMS = [a.split('___')[1] for a in env.action_space.keys() if a.startswith('roll___')]
 
-    plt.plot(avg_random, label='Random')
-    plt.plot(avg_greedy, label='Greedy')
-    plt.plot(avg_ucb1, label='UCB1')
-    plt.xlabel('Timestep')
-    plt.ylabel('Cumulative Regret')
-    plt.legend()
-    plt.title('Bandit Algorithms Regret Comparison')
-    plt.grid(True)
-    plt.show()
+# True mean rewards
+arm_probs = [(i + 1) / (NUM_ARMS + 1) for i in range(NUM_ARMS)]
+mu_star = max(arm_probs)  # True best expected reward
+
+random_regrets = []
+greedy_regrets = []
+ucb1_regrets = []
+
+for _ in range(EPISODES):
+    random_regrets.append(run_random_policy(env))
+    greedy_regrets.append(run_greedy_policy(env))
+    ucb1_regrets.append(run_ucb1_policy(env))
+
+avg_random = np.mean(random_regrets, axis=0)
+avg_greedy = np.mean(greedy_regrets, axis=0)
+avg_ucb1 = np.mean(ucb1_regrets, axis=0)
+
+plt.figure(figsize=(12, 6))
+plt.plot(avg_random, label='Random')
+plt.plot(avg_greedy, label='Greedy')
+plt.plot(avg_ucb1, label='UCB1')
+plt.xlabel('Timestep')
+plt.ylabel('Cumulative Regret')
+plt.legend()
+plt.title('Bandit Algorithms Regret Comparison (Averaged over 20 Runs)')
+plt.grid(True)
+plt.tight_layout()
+plt.savefig(f'plots/bandit.png', dpi=300)
+plt.close()
